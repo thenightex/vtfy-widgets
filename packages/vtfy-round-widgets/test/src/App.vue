898c69a76dd3a78d00c6a26d0d929f07c12142de
thenightex <s.hanke1316@gmail.com>
2020-06-11 14:27:42 +0200

    add minimal vuetify vue test

---
+++ packages/vtfy-round-widgets/test/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-container>
+      <n-text-area v-model="model.area" label="area" outlined></n-text-area>
+      <n-text-field v-model="model.password" label="password" outlined type="password"></n-text-field>
+      <n-text-field v-model="model.text" label="text" outlined></n-text-field>
+      <n-text-field v-model="model.number" label="number" outlined type="number"></n-text-field>
+      <n-date-field v-model="model.date" label="date" outlined output-format="YYYY-MM-DD"></n-date-field>
+      <n-time-field v-model="model.time" label="time" outlined output-format="HH:mm"></n-time-field>
+      <pre>{{ JSON.stringify(model) }}</pre>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      visible: false,
+      model: {
+        area: null,
+        password: null,
+        text: null,
+        number: null,
+        date: null,
+        time: null,
+      },
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ packages/vtfy-round-widgets/test/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/es5';
+import VtfyRoundWidgets from '../../dist/vtfy-round-widgets';
+import App from './App.vue'
+import '@mdi/font/css/materialdesignicons.css';
+
+Vue.config.productionTip = false
+
+// register lib
+Vue.use(VtfyRoundWidgets);
+Vue.use(Vuetify);
+
+new Vue({
+  vuetify: new Vuetify({icons: {iconfont: 'mdi'}}),
+  render: (h) => h(App),
+}).$mount('#app')
